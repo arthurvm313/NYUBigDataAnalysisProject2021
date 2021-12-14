ae6063749182017d9af1a07101f9f00f1028401b
arthurvm313 <45207390+arthurvm313@users.noreply.github.com>
2021-12-13 21:47:49 -0500

    Add files via upload

---
+++  CountRecs.java
@@ -0,0 +1,30 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.File;
+import java.util.*;
+
+
+public class CountRecs {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage:  CountRecs <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(CountRecs.class);
+        job.setJobName(" CountRecs");
+        job.setNumReduceTasks(1);
+        FileOutputFormat.setOutputPath(job, new Path(args[1])); // +"/" + pathname.substring(0,pathname.length()-4))
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        job.setMapperClass(CountRecsMapper.class);
+        job.setReducerClass(CountRecsReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+}
